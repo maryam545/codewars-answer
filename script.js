258ab329a52f30b559a8472f60a54d2fa3fec7c9
maryam <marychch19@gmail.com>
2024-10-01 11:22:28 +0330

    end

--- script.js
+++ script.js
@@ -49,7 +49,8 @@
  *
  *
  */
-function reverseWords(str) {
-  return str.split(" ").reverse().join(" ");
-}
-console.log("hi my name is mary");
+//Reversed Words:
+// function reverseWords(str) {
+//   return str.split(" ").reverse().join(" ");
+// }
+// console.log("hi my name is mary");
